e2579519a799be9a85471af7611d204d85e9b4df
emwalters81 <emwalters81@outlook.com>
2026-03-15 18:23:15 -0400

    Initial Commit

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,25 @@
+
+<!-- TODO: 
+        Add dashboard at top and/or side
+        Add settings functionability after more of the site has been implemented
+        Clean up design -->
+
+
+<script>
+    import Option from './Option.svelte';
+</script>
+
+<main>   
+    <Option optionName="Account Settings" />
+    <Option optionName="Accessibility" />
+</main>
+
+<style>
+
+    main
+    {
+        background-color: lightyellow;
+        min-height: 100vh;
+    }
+
+</style>
---
+++ src/routes/settings/Option.svelte
@@ -0,0 +1,75 @@
+<script>
+    export let optionName;
+
+    let showDropdown = false;
+
+    function toggleDropdown() {
+        showDropdown = !showDropdown;
+    }
+</script>
+
+<div class="container">
+    {optionName}
+
+    <button on:click={toggleDropdown}>&#8615;</button>
+
+    {#if showDropdown && optionName === "Account Settings"}
+        <div class = "dropdown">
+            <p>Profile</p>
+            <p>Security</p>
+            <p>Log Out</p>
+        </div>
+
+    {:else if showDropdown && optionName === "Accessibility"}
+        <div class = "dropdown">
+            <p>Dark Mode</p>
+            <p>Military Time</p>
+            <p></p>
+        </div>
+
+
+    {/if}
+</div>
+
+<style>
+    .container {
+        display: inline-block;
+        font-size: 30px;
+        font-family: "Lucida Console", Monaco, "Courier New", monospace;
+        padding: 20px 40px 20px 20px;
+        margin: 100px 150px;
+        border: 0.1em solid black;
+        border-radius: 20px;
+        background-color: lightgray;
+        position: relative;
+        max-width: 600px;
+    }
+
+    button {
+        position: absolute;
+        bottom: 20px;
+        right: 20px;
+        cursor: pointer;
+    }
+
+    .dropdown 
+    {
+        margin-top: 15px;
+        background: #f0f0f0;
+        border-radius: 10px;
+        padding: 10px;
+    }
+
+
+    .dropdown p 
+    {
+        margin: 5px 0;
+        cursor: pointer;
+    }
+
+    .dropdown p:hover
+    {
+        background-color: lightblue;
+    }
+
+</style>
