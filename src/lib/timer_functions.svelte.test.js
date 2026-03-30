76e5e6e0b1d602d73c5272db62545d9a7929bce3
Isaac Lanning <il225023@ohio.edu>
2026-03-30 00:40:11 -0400

    i dislike unit tests
    i get too focused on the little details and miss deadlines

---
+++ src/lib/timer_functions.svelte.test.js
@@ -0,0 +1,127 @@
+import { flushSync } from 'svelte';
+import { expect, test } from 'vitest';
+import { floor, msToHr, msToMin, msToSec, toggle} from './timer_functions.svelte.js';
+
+
+// 3 tests per function is fine for like 3 timer_functions
+// 5 functions is fine for 2 tests per function
+// doing both is too time consuming for an assignment
+
+// floor will never be negative in timers
+test('Floor Main', () => {
+	let number = 1.5
+  let floored = floor(number)
+
+    expect(floored).toEqual(1)
+});
+
+test('Floor Edge', () => {
+	let number = 1
+  let floored = floor(number)
+
+    expect(floored).toEqual(1)
+});
+
+test('Floor Error', () => {
+	let number = '-1.5'
+  let floored = floor(number)
+
+    expect(floored).toEqual(-2)
+});
+
+
+
+test('msToHr Main', () => {
+  let timer = 152452000
+  let hours = msToHr(timer) 
+
+  expect(hours).toEqual(42)
+});
+
+test('msToHr Edge', () => {
+  let timer = 3600000
+  let hours = msToHr(timer) 
+
+  expect(hours).toEqual(1)
+});
+
+// test for negative timer (unrealisitc)
+test('msToHr Error', () => {
+  let timer = -3600000
+  let hours = msToHr(timer) 
+
+  expect(hours).toEqual('00')
+});
+
+
+
+test('msToMin Main', () => {
+  let timer = 152452000
+  let mins = msToMin(timer) 
+
+  expect(mins).toEqual(20)
+});
+
+// test for negative timer (unrealisitc)
+test('msToMin Edge', () => {
+  let timer = 60000
+  let mins = msToMin(timer) 
+
+  expect(mins).toEqual(1)
+});
+
+// test for negative timer (unrealisitc)
+test('msToMin Error', () => {
+  let timer = -3600000
+  let mins = msToMin(timer) 
+
+  expect(mins).toEqual('00')
+});
+
+
+
+test('msToSec Main', () => {
+  let timer = 152452000
+  let secs = msToMin(timer) 
+
+  expect(secs).toEqual(20)
+});
+
+test('msToSec Edge', () => {
+  let timer = 1000
+  let secs = msToMin(timer) 
+
+  expect(secs).toEqual(1)
+});
+
+// test for negative timer (unrealisitc)
+test('msToSec Error', () => {
+  let timer = -3600000
+  let secs = msToSec(timer) 
+
+  expect(secs).toEqual('00')
+});
+
+
+
+test('Toggle Main', () => {
+  let bool = true
+  let toggled = toggle(bool)
+
+  expect(toggled).toEqual(false)
+});
+
+test('Toggle Edge', () => {
+  let bool = false 
+  let toggled = toggle(bool)
+
+  expect(toggled).toEqual(true)
+});
+
+// hope to not change value
+test('Toggle Error', () => {
+  let bool = '1'
+  let toggled = toggle(bool)
+
+  expect(toggled).toEqual(bool)
+});
