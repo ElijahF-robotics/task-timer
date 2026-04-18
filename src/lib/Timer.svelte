f1a9668f1fa03ab43ca81b100910221b7deecc0d
Isaac Lanning <il225023@ohio.edu>
2026-04-18 15:36:10 -0400

    timer needs refactored. will combat a major logical flaw
    the timeAdd function made me realize we really needed to store elapsed time, and that is a whole thing i just figured out. This wouldn't be so bad if it wasn't integrated into another component and page...

--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -2,17 +2,21 @@
   import { msToHr, msToMin, msToSec} from './timer_functions.svelte.js';
 
   let { timer, timeUp, timeAdd } = $props(); // timeAdd requires focus_timer jank
-  let paused = $state(true)
-  let done = $state(false)
+
+  let elapsed = $state(0) // time elapsed for mid timer additions
+  let paused = $state(true) // for checking if timer is running
+  let done = $state(false) // for checking if time is up
+
 
  $effect(() => {
     if (!paused && timer > -1) { // countdown past 0 to eval done
     
       const interval = window.setInterval(() => {
-        timer -= 125; // remove 1/4 a second
-      }, 125); // for every 1/4 a second
+        // timer -= 125; // remove 1/4 a second
+        elapsed += 125; // give it to elapsed
+      }, 125); // every 1/4 a second
 
-      if (timer <= 0) {done = true} // and sets done when done
+      if (timer-elapsed <= 0) {done = true} // and sets done when done
 
       return () => clearInterval(interval);
     }
@@ -27,14 +31,16 @@
   });
 
   // reset done flag when not done
-  $effect(()=> { if(done && timer > 0) {done = !done} });
+  $effect(()=> { if(done && timer-elapsed > 0) {done = !done} });
 </script>
 
 
 <div class="justify-center content-center text-center">
   <div class="timer_outline">
-    <p>{msToHr(timer)}:{msToMin(timer)}:{msToSec(timer)}</p>
+    <p>{msToHr(timer-elapsed)}:{msToMin(timer-elapsed)}:{msToSec(timer-elapsed)}</p>
   </div>
+  <p>elapsed time: {elapsed}</p>
+  <p>timer: {timer}</p>
   <button onclick={paused = !paused} class="custom_button">
     {#if paused}
       Resume
@@ -44,6 +50,9 @@
   </button>
   {#if paused}
     <button onclick={timeAdd} class="custom_button">Add Time</button>
+    <!-- need to edit actual call to timeAdd -->
+    <!-- remove elapsed time before continuing countdown -->
+    <!-- something like fullTimeAdd() would be great -->
   {/if}
 
 </div>
