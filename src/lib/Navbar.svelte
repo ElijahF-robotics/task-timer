a5b194691f0dc90c441672af85334ef048616ca4
Elijah Flader <elijahflader@gmail.com>
2026-04-24 13:41:45 -0400

    Update the Navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,24 +1,35 @@
 <script>
 	import NavItem from './NavbarItem.svelte';
 	import logo from '$lib/assets/coffee.jpg'
+  	import { page } from '$app/state';
 
 	let { loggedIn } = $props();
+
+	let currentPath = $derived(page.url.pathname)
 </script>
 
-<div class="flex flex-row items-center justify-between p-1.5 border-b border-b-gray-500 fixed top-0 left-0 right-0 z-50">
-	<a href="/"><img src={logo} alt="Placeholder" class="flex-1 h-20 w-auto rounded-2xl" /></a>
+<div class="flex flex-row items-stretch justify-between p-1.5 border-b border-b-gray-500 fixed top-0 left-0 right-0 z-50">
+	<a href="/">
+		<div class="flex flex-row items-center">
+			<img src={logo} alt="Placeholder" class="flex-1 h-15 w-auto rounded-2xl" />
+			<p class="ml-1">Task Timer</p>
+		</div>
+	</a>
+	
 
 	<div class="flex flex-row">
-		<NavItem href="/" text="Tasks"></NavItem>
-		<NavItem href="/grade-calculator" text="Grade Calculator"></NavItem>
-		<NavItem href="/caffeine-calculator" text="Coffee!"></NavItem>
-		<NavItem href="/settings" text="Settings"></NavItem>
+		<NavItem href="/" text="Home" selected={currentPath == "/" || currentPath == "/login"}></NavItem>
+		{#if loggedIn}
+			<NavItem href="/grade-calculator" text="Grade Calculator" selected={currentPath == "/grade-calculator"}></NavItem>
+			<NavItem href="/caffeine-calculator" text="Coffee!" selected={currentPath == "/caffeine-calculator"}></NavItem>
+			<!-- <NavItem href="/settings" text="Settings" selected={currentPath == "/settings"}></NavItem> -->
+		{/if}
 	</div>
 
 	{#if !loggedIn}
-		<NavItem href="/sign-up" text="Sign Up" class="flex-1 justify-end"></NavItem>
+		<NavItem href="/sign-up" text="Sign Up" selected={currentPath == "/sign-up"}></NavItem>
 	{:else}
-		<NavItem href="/settings" text="Profile" class="flex-1 justify-end"></NavItem>
+		<NavItem href="/settings" text="Profile" selected={currentPath == "/settings"}></NavItem>
 	{/if}
 
 
--- src/lib/NavbarItem.svelte
+++ src/lib/NavbarItem.svelte
@@ -1,8 +1,13 @@
 <script>
 	import Button from '$lib/Button.svelte';
-	let {href, text} = $props();
+	let {href, text, selected} = $props();
 </script>
 
 <div>
-	<a {href}><Button {text}></Button></a>
+	<a {href}> 
+		<div class={`flex items-center h-full px-2 mx-2 ${selected ? "border-b-2 font-bold " : ""}`}>
+			{text}
+		</div>
+		
+	</a>
 </div>
